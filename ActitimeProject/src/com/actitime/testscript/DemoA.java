8f844ce8f20540d94c1bb58373b41be942ea1476
Ragini <Ragini@RaginiPC>
2024-10-14 05:49:37 +0530

    added printing statement

--- ActitimeProject/src/com/actitime/testscript/DemoA.java
+++ ActitimeProject/src/com/actitime/testscript/DemoA.java
@@ -16,6 +16,7 @@ public static void main(String[] args) {
 	driver.findElement(By.name("pwd")).sendKeys("manager");
 	driver.findElement(By.xpath("//div[text()='Login ']")).click();
 	driver.findElement(By.linkText("TASKS")).click();
-	System.out.println("Task page is displayed");
+	//System.out.println("Task page is displayed");
+	System.out.println("Task page is not displayed");
 }
 }
